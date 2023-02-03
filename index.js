55acffd1246dc706cdf04f6de09b61f024510def
drake bishop <drakemx85@gmail.com>
2023-02-02 21:19:03 -0600

    commit tree ea8da8ed75b688daa3f292d0408f457a0418adff

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution in this file!
+const pickup1 = (43)
+const pickup2 = (50)
+const pickup3 = (34)
+
+function distanceFromHqInBlocks(pickup1) {
+    if (pickup1 = 43)
+    return (1)
+}
+
+function distanceFromHqInBlocks(pickup2) {
+    return (50, 8)
+}
+
+function distanceFromHqInBlocks(pickup3) {
+    return (34, 8)
+}
